b3b4c0e2e8ed3dea914a92e849975be5e7edb22c
Wendel <wendellduos@hotmail.com>
2024-02-29 23:39:37 -0300

    created initial front end

--- main.js
+++ main.js
@@ -1,100 +1,88 @@
-const fs = require("node:fs");
-const qrcode = require("qrcode-terminal");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const { Client, MessageMedia } = require("whatsapp-web.js");
-const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const app = express();
+const port = 3000;
+let qrdata;
 
-let contactList = [];
-let imagePath = "./content/image.png";
-const captionPath = "./content/caption.txt";
-let caption;
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// read text from caption.txt file
-fs.readFile(captionPath, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  caption = data;
-});
+app.post("/submit-message", (req, res) => {
+  const message = req.body;
 
-// create new client and log qr code
-const client = new Client();
+  let contactList = [];
+  let imagePath = "./content/image.png";
 
-client.on("qr", (qr) => {
-  console.clear();
-  qrcode.generate(qr, { small: true });
-});
+  const client = new Client();
 
-// when ready, says so and asks for user confirmation on message to send
-client.on("ready", () => {
-  console.log("Login efetuado!\n\n");
-  console.warn(
-    "A imagem [image.png] será enviada à todos os seus contatos, com a seguinte legenda:\n"
-  );
-  console.log(`${caption}\n`);
+  client.on("qr", (qr) => {
+    console.log(qr);
+    qrdata = qr;
+  });
 
-  rl.question("Deseja prosseguir? [S/n/teste]", (answer) => {
-    const userChoice = answer.toLowerCase();
-
-    if (userChoice === "n") {
-      console.log("A execução foi cancelada pelo usuário.");
-      process.exit();
-    } else if (userChoice === "teste") {
+  client.on("ready", () => {
+    if (message.for === "just-me") {
       testMessage();
-    } else {
+    } else if (message.for === "all-contacts") {
       sendMessages();
     }
   });
+
+  client.initialize();
+
+  // gets contact list and sends messages
+  async function sendMessages() {
+    const contacts = await client.getContacts();
+
+    contacts.forEach((contact) => {
+      if (contact.isWAContact && contact.isMyContact) {
+        contactList.push(contact.id._serialized);
+      }
+    });
+
+    contactList.forEach((id) => {
+      const image = MessageMedia.fromFilePath(imagePath);
+      client.sendMessage(id, image, {
+        caption: message.body,
+      });
+    });
+  }
+
+  // sends message to user's own number
+  async function testMessage() {
+    const contacts = await client.getContacts();
+
+    let myId;
+
+    // get user's own id/number
+    contacts.forEach((contact) => {
+      if (contact.isMe) {
+        myId = contact.id._serialized;
+      }
+    });
+
+    const image = MessageMedia.fromFilePath(imagePath);
+
+    client.sendMessage(myId, image, {
+      caption: message.body,
+    });
+  }
+
+  res.json({
+    message: "Message received by the server!",
+  });
 });
 
-client.initialize();
-
-// gets contact list and sends messages
-async function sendMessages() {
-  const contacts = await client.getContacts();
-
-  contacts.forEach((contact) => {
-    if (contact.isWAContact && contact.isMyContact) {
-      contactList.push(contact.id._serialized);
-    }
+app.post("/request-qrcode", (req, res) => {
+  res.json({
+    qr: qrdata,
   });
+});
 
-  contactList.forEach((id) => {
-    const image = MessageMedia.fromFilePath(imagePath);
-    client.sendMessage(id, image, {
-      caption: caption,
-    });
-  });
-
-  console.log(
-    "\n\nEm processo.\n\nPressione Ctrl+C para finalizar a execução APÓS a conclusão dos envios."
-  );
-}
-
-// sends message to user's own number
-async function testMessage() {
-  console.log("A mensagem será enviada somente ao seu próprio número.");
-
-  const contacts = await client.getContacts();
-  let myId;
-
-  contacts.forEach((contact) => {
-    if (contact.isMe) {
-      myId = contact.id._serialized;
-    }
-  });
-
-  const image = MessageMedia.fromFilePath(imagePath);
-  client.sendMessage(myId, image, {
-    caption: caption,
-  });
-
-  console.log(
-    "\n\nEm processo.\n\nPressione Ctrl+C para finalizar a execução APÓS a conclusão do envio."
-  );
-}
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
